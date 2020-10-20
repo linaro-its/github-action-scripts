ace35cd6feb6052a047cbaeab0c2ad8cbe08ffea
Philip Colmer <philip.colmer@linaro.org>
2020-10-20 15:56:29 +0100

    Add script

---
+++ close-pr.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+PR_NUMBER=$(jq -r ".pull_request.number" $GITHUB_EVENT_PATH)
+echo "Cleaning up after $PR_NUMBER has closed"
+BUILDDIR="$AWS_STATIC_SITE_URL-$PR_NUMBER"
+if [ -d "$BUILDDIR" ]; then
+    echo "Removing website review directory"
+    rm -rf "$BUILDDIR"
+else
+    echo "No website review directory to remove"
+fi
+A11YDIR="/srv/a11y.linaro.org/$AWS_STATIC_SITE_URL-$PR_NUMBER.websitepreview.linaro.org"
+if [ -d "$A11YDIR" ]; then
+    echo "Removing $A11YDIR"
+    rm -rf "$A11YDIR"
+else
+    echo "No a11y test to remove"
+fi

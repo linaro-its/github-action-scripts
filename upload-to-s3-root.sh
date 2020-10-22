702a503abaa47ec8875bac400180219cc20fd9b1
Philip Colmer <philip.colmer@linaro.org>
2020-10-22 14:54:15 +0100

    Fix execution of Python script

--- upload-to-s3-root.sh
+++ upload-to-s3-root.sh
@@ -4,4 +4,5 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "/srv/s3-staging/$SITE_URL" || exit 1
 aws --profile "$AWS_STATIC_SITE_PROFILE" s3 sync --cache-control "public, max-age=86400" ./ "s3://$AWS_STATIC_SITE_URL" --delete
-$DIR/set_last_modified_meta.py
+cd $DIR
+pipenv run python set_last_modified_meta.py

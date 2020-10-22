1017f3800d5b0aad255031bbd83115b52a33b6a7
Philip Colmer <philip.colmer@linaro.org>
2020-10-22 14:02:16 +0100

    Fix jq & allow path to be specified

--- check-links.sh
+++ check-links.sh
@@ -3,23 +3,14 @@
 #
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PR_NUMBER=$(jq -r ".pull_request.number" $GITHUB_EVENT_PATH)
-STATUSES_URL=$(jq -r ".pull_request.statuses_url" $GITHUB_EVENT_PATH)
+STATUSES_URL=$(jq -r ".pull_request.statuses_url // empty" $GITHUB_EVENT_PATH)
 
-if [ "$STATUSES_URL" == "" ]; then
-  # Do not run link checks when this isn't a test build. Have to mark it as successful as there isn't
-  # a GitHub status of skipped.
-  echo "Skipping link checks"
-  touch "$GITHUB_WORKSPACE/../linktests-success"
-  exit 0
-fi
-
-# Note: do NOT use "set -e" in this script because we need the "if" statement to execute and it won't
-# if we use "set -e"
-
-# If we're running a test, the built site gets moved outside of the working direcory
-if [ "$STATUSES_URL" == "" ]; then
-  BUILDDIR="$GITHUB_WORKSPACE/$SITE_URL"
+if [ -z "$STATUSES_URL" ]; then
+  # If we're not running a test, the built site is likely to be in the cache
+  # directory so we get passed the path on the command line.
+  BUILDDIR="$1"
 else
+  # If we're running a test, the built site gets moved outside of the working direcory
   BUILDDIR="/srv/websitepreview/$AWS_STATIC_SITE_URL-$PR_NUMBER"
 fi
 

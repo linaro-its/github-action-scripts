12d058ba226796beb794a4c290b4235e392fdef9
Philip Colmer <philip.colmer@linaro.org>
2020-10-22 15:20:35 +0100

    Fix parameter parsing

--- set-up-lambda-redirect.sh
+++ set-up-lambda-redirect.sh
@@ -2,15 +2,15 @@
 # shellcheck disable=SC2154
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-if [ "$#" == 1 ]; then
-    # No parameters - just exit
-    exit 0
-fi
-if [ ! -d "$1" ]; then
-    echo "$1 is not a directory"
-    exit 1
-fi
-if [ -d "$1/_data" ] && [ -f "$1/_data/routingrules.json" ]; then
-    cd $DIR
-    pipenv run python lambda-redirect.py -r "$1/_data/routingrules.json"
+if [ -n "$1" ]; then
+    if [ ! -d "$1" ]; then
+        echo "$1 is not a directory"
+        exit 1
+    fi
+    if [ -d "$1/_data" ] && [ -f "$1/_data/routingrules.json" ]; then
+        cd $DIR
+        pipenv run python lambda-redirect.py -r "$1/_data/routingrules.json"
+    fi
+else
+    echo "No parameters provided - skipping"
 fi

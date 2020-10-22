3c169239e3c1408e5d453c29c5e89349c364464e
Philip Colmer <philip.colmer@linaro.org>
2020-10-22 15:33:08 +0100

    Add scripts for final push jobs

---
+++ lambda-security-headers.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python3
+
+
+import sys
+import os
+import boto3
+
+
+_PROFILE = (
+    "AWS_STATIC_SITE_PROFILE"
+)
+_CFDIST = (
+    "CF_DIST_ID_STATIC_LO"
+)
+_LAMBDA_FUNC = (
+    "CLOUDFRONT_ADD_SECURITY_HEADERS_ARN"
+)
+
+
+def get_env_var(env):
+    foo = os.environ.get(env)
+    if foo is None:
+        print("Cannot retrieve environment variable '%s'" % env)
+        sys.exit(1)
+    return foo
+
+
+def lambda_list_functions():
+    profile = get_env_var(_PROFILE)
+    session = boto3.Session(profile_name=profile)
+    client = session.client('lambda', 'us-east-1')
+    return client.list_functions()["Functions"]
+
+
+def get_function():
+    functions = lambda_list_functions()
+    for f in functions:
+        if f["FunctionName"] == "cloudfront-add-security-headers":
+            return f
+    # Failed to find the Lambda function
+    sys.exit(1)
+
+
+def is_function_attached():
+    func = get_env_var(_LAMBDA_FUNC)
+    profile = get_env_var(_PROFILE)
+    session = boto3.Session(profile_name=profile)
+    cf_client = session.client('cloudfront', 'us-east-1')
+    config = cf_client.get_distribution_config(
+        Id=get_env_var(_CFDIST)
+    )
+    dc = config["DistributionConfig"]
+    dcb = dc["DefaultCacheBehavior"]
+    if ("LambdaFunctionAssociations" not in dcb or
+            "Items" not in dcb["LambdaFunctionAssociations"]):
+        return False
+    for lfa in dcb["LambdaFunctionAssociations"]["Items"]:
+        if lfa["LambdaFunctionARN"] == func:
+            return True
+    return False
+
+
+def attach_function():
+    # Get the function ARN without the version number in it
+    func = get_env_var(_LAMBDA_FUNC)
+    no_ver = func.rsplit(":", 1)[0]
+
+    item_block = {
+        "EventType": "origin-response",
+        "LambdaFunctionARN": func
+    }
+    profile = get_env_var(_PROFILE)
+    session = boto3.Session(profile_name=profile)
+    cf_client = session.client('cloudfront', 'us-east-1')
+    config = cf_client.get_distribution_config(
+        Id=get_env_var(_CFDIST)
+    )
+    dc = config["DistributionConfig"]
+    dcb = dc["DefaultCacheBehavior"]
+    if "LambdaFunctionAssociations" not in dcb:
+        dcb["LambdaFunctionAssociations"] = {
+            "Items": [
+                item_block
+            ],
+            "Quantity": 1
+        }
+        print("Initialising LFA block to add security headers function")
+    else:
+        found = False
+        if "Items" not in dcb["LambdaFunctionAssociations"]:
+            dcb["LambdaFunctionAssociations"]["Items"] = []
+        else:
+            for lfa in dcb["LambdaFunctionAssociations"]["Items"]:
+                if lfa["LambdaFunctionARN"].startswith(no_ver):
+                    lfa["LambdaFunctionARN"] = func
+                    found = True
+                    print("Updating existing security headers function")
+                    break
+        if not found:
+            lfa_block = dcb["LambdaFunctionAssociations"]
+            lfa_block["Items"].append(item_block)
+            lfa_block["Quantity"] = lfa_block["Quantity"] + 1
+            print(
+                "Adding security headers function, total of %s" %
+                lfa_block["Quantity"])
+    cf_client.update_distribution(
+        DistributionConfig=dc,
+        Id=get_env_var(_CFDIST),
+        IfMatch=config["ETag"]
+    )
+    print("CloudFront distribution updated")
+
+
+if __name__ == '__main__':
+    if is_function_attached():
+        print("Security headers function is already attached")
+    else:
+        attach_function()

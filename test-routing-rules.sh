83b177119059ede9d59bac181ff503e800e3115f
Philip Colmer <philip.colmer@linaro.org>
2020-10-28 12:18:44 +0000

    Move website repo dir

--- test-routing-rules.sh
+++ test-routing-rules.sh
@@ -4,7 +4,7 @@
 # Note: do NOT use "set -e" in this script because we need the "if" statement to execute and it won't
 # if we use "set -e"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd "$GITHUB_WORKSPACE" || exit 1
+cd "$GITHUB_WORKSPACE/website" || exit 1
 if [ -d _data ] && [ -f _data/routingrules.json ]; then
   $DIR/test-routing-rules.py
 else

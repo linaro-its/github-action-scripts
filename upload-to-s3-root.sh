2db69b2ee357f01c5492747726ba1e8638f528fa
Philip Colmer <philip.colmer@linaro.org>
2020-12-17 10:12:16 +0000

    Change cache settings for HTML files.

--- upload-to-s3-root.sh
+++ upload-to-s3-root.sh
@@ -3,6 +3,10 @@
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "/srv/s3-staging/$SITE_URL" || exit 1
-aws --profile "$AWS_STATIC_SITE_PROFILE" s3 sync --cache-control "public, max-age=86400" ./ "s3://$AWS_STATIC_SITE_URL" --delete
+# Sync HTML files with different cache settings. Using "no-cache" does *NOT* mean that the file is not
+# cached - it just forces the browser to do a quick check upstream to make sure that the page is valid.
+aws --profile "$AWS_STATIC_SITE_PROFILE" s3 sync --exclude "*" --include "*.html" --cache-control "no-cache, max-age=86400" ./ "s3://$AWS_STATIC_SITE_URL" --delete
+# Sync non-HTML files with "normal" cache settings.
+aws --profile "$AWS_STATIC_SITE_PROFILE" s3 sync --include "*" --exclude "*.html" --cache-control "public, max-age=86400" ./ "s3://$AWS_STATIC_SITE_URL" --delete
 cd $DIR
 pipenv run python set_last_modified_meta.py

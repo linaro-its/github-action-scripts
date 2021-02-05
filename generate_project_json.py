bbd34681c9dfb820ff2cb30a51c86ef1c9726748
Philip Colmer <philip.colmer@linaro.org>
2021-02-05 10:19:29 +0000

    Fix pylint issues

--- generate_project_json.py
+++ generate_project_json.py
@@ -1,3 +1,4 @@
+""" Generate the JSON file used to drive the Projects data. """
 import os
 import shutil
 import sys
@@ -9,10 +10,11 @@ from requests.auth import HTTPBasicAuth
 import json_generation_lib
 
 PI_SLUG = "Project Information"
-nesting_level = 0
+NESTING_LEVEL = 0
 
 
 def initialise_ldap():
+    """ Return a LDAP Connection. """
     username = "cn=bamboo-bind,ou=binders,dc=linaro,dc=org"
     password = json_generation_lib.get_vault_secret("secret/ldap/{}".format(username))
     return Connection(
@@ -24,12 +26,14 @@ def initialise_ldap():
 
 
 def initialise_auth():
+    """ Return a HTTP Auth. """
     username = "it.support.bot"
     password = json_generation_lib.get_vault_secret("secret/ldap/{}".format(username))
     return HTTPBasicAuth(username, password)
 
 
 def jira_get(url, jira_auth):
+    """ Fetch from the Jira Projects server. """
     headers = {'content-type': 'application/json'}
     try:
         response = requests.get(
@@ -39,46 +43,54 @@ def jira_get(url, jira_auth):
             print("Getting %s failed with code %s" % (url, response.status_code))
             sys.exit(1)
         return response.json()
-    except Exception as e:
-        print("While fetching %s, got exception: %s" % (url, str(e)))
+    except Exception as exc: # pylint: disable=broad-except
+        print("While fetching %s, got exception: %s" % (url, str(exc)))
         sys.exit(1)
 
 
 def get_all_projects(jira_auth):
+    """ Get all of the Jira projects. """
     return jira_get("rest/api/2/project", jira_auth)
 
 
 def meta_value(meta_data, key, group="Project Visibility"):
-    for m in meta_data:
-        if m["key"] == key and m["group"] == group:
-            return m["value"]
+    """ Return the value for a given meta key and group. """
+    for meta in meta_data:
+        if meta["key"] == key and meta["group"] == group:
+            return meta["value"]
     return ""
 
 
 def get_metadata(jira_projects, jira_auth):
+    """
     # Iterate through the projects, looking for projects
     # that have got metadata defined.
+    """
     meta_results = {}
-    for p in jira_projects:
+    for proj in jira_projects:
         meta = jira_get(
-            "rest/metadata/latest/project/%s?includeHidden=true" % p["key"], jira_auth)
+            "rest/metadata/latest/project/%s?includeHidden=true" % proj["key"], jira_auth)
         # Only include projects thare are active, open and published
         if meta != []:
             pv_open = meta_value(meta, "Open")
             pv_active = meta_value(meta, "Active")
             pv_published = meta_value(meta, "Published")
             pv_visibility = meta_value(meta, "property_visibility", "system")
-            if pv_open == "Yes" and pv_active == "Yes" and pv_published == "Yes" and pv_visibility != "":
-                meta_results[p["key"]] = meta
+            if (pv_open == "Yes" and
+                    pv_active == "Yes" and
+                    pv_published == "Yes" and
+                    pv_visibility != ""):
+                meta_results[proj["key"]] = meta
             else:
                 print("Ignoring %s - open='%s', active='%s', published='%s', visibility='%s'" % (
-                    p["key"], pv_open, pv_active, pv_published, pv_visibility))
+                    proj["key"], pv_open, pv_active, pv_published, pv_visibility))
         else:
-            print("Ignoring %s - no metadata" % p["key"])
+            print("Ignoring %s - no metadata" % proj["key"])
     return meta_results
 
 
 def get_specific_projects(metadata, jira_auth):
+    """ For projects specified in the metadata, get the corresponding Jira data. """
     results = []
     for key in metadata.keys():
         project = jira_get(
@@ -88,7 +100,7 @@ def get_specific_projects(metadata, jira_auth):
 
 
 def lookup_email(email):
-    # Try to get a display name back for the given email address.
+    """ Try to get a display name back for the given email address. """
     with initialise_ldap() as ldap_conn:
         if ldap_conn.search(
             "dc=linaro,dc=org",
@@ -100,9 +112,11 @@ def lookup_email(email):
 
 
 def htmlise_email(email):
-    # If the email address ends with a full-stop, remove it
-    # before wrapping tags around and then add it back
-    # afterwards.
+    """
+    If the email address ends with a full-stop, remove it
+    before wrapping tags around and then add it back
+    afterwards.
+    """
     if email[-1] == ".":
         got_fullstop = True
         email = email[:-1]
@@ -112,13 +126,16 @@ def htmlise_email(email):
     if name is None:
         result = "<a href=\"mailto:%s\">%s</a>" % (email, email)
     else:
-        result = "%s <a class=\"email-icon\" href=\"mailto:%s\"><span class=\"icon-mail\"></span></a>" % (name, email)
+        result = (
+            "%s <a class=\"email-icon\" href=\"mailto:%s\">"
+            "<span class=\"icon-mail\"></span></a>" % (name, email))
     if got_fullstop:
         result += "."
     return result
 
 
 def htmlise_markdown(url):
+    """ Convert a markdown URL into a HTML URL """
     # Split on the |
     parts = url.split("|")
     if len(parts) != 2:
@@ -126,9 +143,10 @@ def htmlise_markdown(url):
     part1 = parts[0][1:]
     part2 = parts[1][:-1]
     return "<a href=\"%s\">%s</a>" % (part2, part1)
-    
+
 
 def htmlise_url(url):
+    """ Convert a URL into HTML tags. """
     # Does the URL look like markdown?
     if url[0] == "[":
         return htmlise_markdown(url)
@@ -147,6 +165,7 @@ def htmlise_url(url):
 
 
 def find_markers(line, known_point, start_char, end_char, make_sane=True):
+    """ Find the specified chars in the line. """
     start = line.rfind(start_char, 0, known_point)
     end = line.find(end_char, known_point)
     if make_sane:
@@ -163,6 +182,7 @@ def find_markers(line, known_point, start_char, end_char, make_sane=True):
 
 
 def process_email(at_pos, line, result):
+    """ Extract and convert an email address to HTML. """
     start, end = find_markers(line, at_pos, " ", " ")
     # Now extract anything before 'start'
     if start != 0:
@@ -176,6 +196,7 @@ def process_email(at_pos, line, result):
 
 
 def process_url(url_pos, line, result):
+    """ Extract and convert a URL to HTML. """
     # This is slightly complicated by the fact that
     # we need to support Jira link markdown which
     # can support spaces in the readable text, so
@@ -198,11 +219,12 @@ def process_url(url_pos, line, result):
 
 
 def htmlise_unordered_list(line):
-    global nesting_level
+    """ Convert an unordered list to HTML. """
+    global NESTING_LEVEL # pylint: disable=global-statement
     # Before we do anything else, if the current nesting
     # level is non-zero, close off the previous list entry.
     result = ""
-    if nesting_level != 0:
+    if NESTING_LEVEL != 0:
         result = "</li>"
     # How many stars are there? We split on the first space
     # which should come after all of the stars.
@@ -211,21 +233,26 @@ def htmlise_unordered_list(line):
     # that everything up to the space is also * and that is
     # our nesting level.
     level = len(parts[0])
-    if nesting_level < level:
+    if NESTING_LEVEL < level:
         # Start a new list
         result += "<ul>"
-    elif nesting_level > level:
+    elif NESTING_LEVEL > level:
         # End the previous list. Note that we DON'T append this
         # (unlike starting a new list) because HTML requires
         # the list to end before the list entry is ended.
         result = "</ul></li>"
     # Now start this list entry
     result += "<li>"
-    nesting_level = level
+    NESTING_LEVEL = level
     return result + " " + htmlise_non_list_line(parts[1])
 
 
 def htmlise_non_list_line(line):
+    """
+    Process a line that isn't in a list, looking for email
+    addresses and URLs so that they can be converted into
+    the appropriate HTML.
+    """
     result = ""
     while True:
         at_pos = line.find("@")
@@ -246,7 +273,8 @@ def htmlise_non_list_line(line):
 
 
 def htmlise_line(line):
-    global nesting_level
+    """ Scan the line, converting segments to HTML where needed. """
+    global NESTING_LEVEL # pylint: disable=global-statement
     result = ""
 
     if line == "":
@@ -255,20 +283,21 @@ def htmlise_line(line):
         # If the line is part of an unordered list, process the list
         # part first and then process the rest of the line.
         return htmlise_unordered_list(line)
-    elif nesting_level != 0:
+    elif NESTING_LEVEL != 0:
         # We've got a non-list line and the nesting level is
         # non-zero, so decrement the nesting level and close off
         # a list.
-        nesting_level -= 1
+        NESTING_LEVEL -= 1
         result = "</li></ul>"
     return result + htmlise_non_list_line(line)
 
 
 def htmlise_value(value):
-    global nesting_level
+    """ Process the value, converting appropriate segments to HTML. """
+    global NESTING_LEVEL # pylint: disable=global-statement
     # The nesting level should already be zero because we
     # decrement it as the list ends but just in case ...
-    nesting_level = 0
+    NESTING_LEVEL = 0
 
     # Break the value into lines. If there is only one
     # line then just process it straight away. Otherwise
@@ -279,23 +308,24 @@ def htmlise_value(value):
         return htmlise_line(value.strip("\r"))
 
     result = ""
-    for p in parts:
-        if result != "" and nesting_level == 0:
+    for par in parts:
+        if result != "" and NESTING_LEVEL == 0:
             result += "<br>"
-        result += htmlise_line(p.strip("\r"))
+        result += htmlise_line(par.strip("\r"))
 
     # Make sure we don't have an open list
-    while nesting_level != 0:
+    while NESTING_LEVEL != 0:
         result += "</li></ul>"
-        nesting_level -= 1
+        NESTING_LEVEL -= 1
     return result
 
 
 def construct_project_data(projects, metadata):
+    """ Convert the separate project data into a single Python object. """
     results = []
-    for p in projects:
-        if p["key"] in metadata:
-            blob = construct_project_blob(p, metadata)
+    for proj in projects:
+        if proj["key"] in metadata:
+            blob = construct_project_blob(proj, metadata)
             results.append(blob)
     # Sort the projects by title
     results = sorted(results, key=lambda x: x[PI_SLUG]["title"])
@@ -304,12 +334,13 @@ def construct_project_data(projects, metadata):
     }
 
 
-def construct_project_blob(p, metadata):
+def construct_project_blob(proj, metadata):
+    """ Construct the per-project Python object. """
     blob = {
-        "key": p["key"],
-        "icon": p["avatarUrls"]["48x48"]
+        "key": proj["key"],
+        "icon": proj["avatarUrls"]["48x48"]
     }
-    meta = metadata[p["key"]]
+    meta = metadata[proj["key"]]
     property_list = meta_value(meta, "property_visibility", "system")
     properties = property_list.split("\n")
     # Now add the values
@@ -323,12 +354,16 @@ def construct_project_blob(p, metadata):
     # Finish off with the title and description from the project
     if PI_SLUG not in blob:
         blob[PI_SLUG] = {}
-    blob[PI_SLUG]["title"] = htmlise_value(p["name"])
-    blob[PI_SLUG]["description"] = htmlise_value(p["description"])
+    blob[PI_SLUG]["title"] = htmlise_value(proj["name"])
+    blob[PI_SLUG]["description"] = htmlise_value(proj["description"])
     return blob
 
 
 def check_project_dir_exists(key, projects_directory):
+    """
+    Ensure that the directory exists for the specified project and,
+    if it doesn't, create and initialise the posts file.
+    """
     path = "%s/%s" % (projects_directory, key.lower())
     if os.path.isdir(path):
         return
@@ -343,6 +378,10 @@ def check_project_dir_exists(key, projects_directory):
 
 
 def sync_project_pages(project_data, projects_directory):
+    """
+    Ensure project directories exist for projects that exist, and remove
+    directories for those projects that no longer exist.
+    """
     # Below _pages/projects, there is a directory for each project (lower-case name) and,
     # within that, a file called "posts.md" with this structure:
     #
@@ -354,18 +393,19 @@ def sync_project_pages(project_data, projects_directory):
     # ---
     projects = project_data["projects"]
     project_keys_lower = []
-    for p in projects:
-        check_project_dir_exists(p["key"], projects_directory)
-        project_keys_lower.append(p["key"].lower())
+    for proj in projects:
+        check_project_dir_exists(proj["key"], projects_directory)
+        project_keys_lower.append(proj["key"].lower())
     #
     # Remove any directories that exist for projects that don't ...
     subdirs = [f.name for f in os.scandir(projects_directory) if f.is_dir()]
-    for s in subdirs:
-        if s not in project_keys_lower:
-            shutil.rmtree("%s/%s" % (projects_directory, s))
+    for sub in subdirs:
+        if sub not in project_keys_lower:
+            shutil.rmtree("%s/%s" % (projects_directory, sub))
 
 
 def main():
+    """ Main code. """
     jira_auth = initialise_auth()
     jira_projects = get_all_projects(jira_auth)
     if len(jira_projects) == 0:
@@ -379,7 +419,8 @@ def main():
     project_data = construct_project_data(jira_projects, jira_metadata)
     working_dir = json_generation_lib.working_dir()
     sync_project_pages(project_data, "%s/website/_pages/projects" % working_dir)
-    json_generation_lib.do_the_git_bits(project_data, "%s/website/_data/projects.json" % working_dir)
+    json_generation_lib.do_the_git_bits(
+        project_data, "%s/website/_data/projects.json" % working_dir)
 
 
 if __name__ == '__main__':

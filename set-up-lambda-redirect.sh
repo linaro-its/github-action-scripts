a1f2dec41310b69e68ba6be3af4c402401993621
Philip Colmer <philip.colmer@linaro.org>
2021-11-25 07:56:31 +0000

    Remove double-slash

--- set-up-lambda-redirect.sh
+++ set-up-lambda-redirect.sh
@@ -7,10 +7,10 @@ if [ -n "$1" ]; then
         echo "$1 is not a directory"
         exit 1
     fi
-    if [ -d "$1/_data" ] && [ -f "$1/_data/routingrules.json" ]; then
+    if [ -d "$1_data" ] && [ -f "$1_data/routingrules.json" ]; then
         cd $DIR
-        echo "Processing $1/_data/routingrules.json"
-        pipenv run python lambda_redirect.py -r "$1/_data/routingrules.json"
+        echo "Processing $1_data/routingrules.json"
+        pipenv run python lambda_redirect.py -r "$1_data/routingrules.json"
     else
         echo "No routing rules - skipping"
     fi

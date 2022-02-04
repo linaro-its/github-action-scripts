6c9678925d20fab44621ebac0ce0ca0f54bec36f
Philip Colmer <philip.colmer@linaro.org>
2022-02-04 08:16:59 +0000

    Cope with dirs not ending with /

--- set-up-lambda-redirect.sh
+++ set-up-lambda-redirect.sh
@@ -3,14 +3,21 @@
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 if [ -n "$1" ]; then
-    if [ ! -d "$1" ]; then
-        echo "$1 is not a directory"
+    PARM1="$1"
+    # Some push workflows don't provide the / at the end so check and
+    # add if missing.
+    LAST="${$PARM1: -1}"
+    if [ "$LAST" != "/" ]; then
+        PARM1="$PARM1/"
+    fi
+    if [ ! -d "$PARM1" ]; then
+        echo "$PARM1 is not a directory"
         exit 1
     fi
-    if [ -d "$1_data" ] && [ -f "$1_data/routingrules.json" ]; then
+    if [ -d "$PARM1_data" ] && [ -f "$PARM1_data/routingrules.json" ]; then
         cd $DIR
-        echo "Processing $1_data/routingrules.json"
-        pipenv run python lambda_redirect.py -r "$1_data/routingrules.json"
+        echo "Processing $PARM1_data/routingrules.json"
+        pipenv run python lambda_redirect.py -r "$PARM1_data/routingrules.json"
     else
         echo "No routing rules - skipping"
     fi

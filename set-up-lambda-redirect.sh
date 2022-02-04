d6b9f1bc076ee30c2661ebe7863a36b39f2465f7
Philip Colmer <philip.colmer@linaro.org>
2022-02-04 08:25:16 +0000

    Rework the string logic

--- set-up-lambda-redirect.sh
+++ set-up-lambda-redirect.sh
@@ -4,20 +4,22 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 if [ -n "$1" ]; then
     PARM1="$1"
-    # Some push workflows don't provide the / at the end so check and
-    # add if missing.
+    # Some push workflows provide a / at the end so check and remove
+    # if present
     LAST="${PARM1: -1}"
-    if [ "$LAST" != "/" ]; then
-        PARM1="$PARM1/"
+    if [ "$LAST" == "/" ]; then
+        PARM1="${PARM1%?}"
     fi
     if [ ! -d "$PARM1" ]; then
         echo "$PARM1 is not a directory"
         exit 1
+    else
+        echo "Checking $PARM1 for routing rules"
     fi
-    if [ -d "$PARM1_data" ] && [ -f "$PARM1_data/routingrules.json" ]; then
+    if [ -d "$PARM1/_data" ] && [ -f "$PARM1/_data/routingrules.json" ]; then
         cd $DIR
-        echo "Processing $PARM1_data/routingrules.json"
-        pipenv run python lambda_redirect.py -r "$PARM1_data/routingrules.json"
+        echo "Processing $PARM1/_data/routingrules.json"
+        pipenv run python lambda_redirect.py -r "$PARM1/_data/routingrules.json"
     else
         echo "No routing rules - skipping"
     fi
